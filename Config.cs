bcfb76ba8383ee6569ffc92808eff43093657577
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-08-02 16:14:07 +0900

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Loader;
+using Exiled.API.Interfaces;
+
+namespace MERDummyDoorInstaller
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public List<string> DummyDoorInstallingMaps { get; set; } = new List<string>
+        {
+            "ExampleMap"
+        };
+        [Description("Below index means the door numbering that do not require dummy door installing. Caution: It's double List. - (Above Map Index) - (Door Index)")]
+        public List<List<int>> BlackListOfDummyDoor { get; set; } = new List<List<int>>
+        {
+            new List<int>
+            {
+                101
+            },
+            new List<int>
+            {
+                15
+            }
+        };
+    }
+}
---
+++ MERDummyDoorInstaller.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using Exiled.Loader;
+using UnityEngine;
+
+using Maps = MapEditorReborn.Events.Handlers.Map;
+
+namespace MERDummyDoorInstaller
+{
+    public class MERDummyDoorInstaller : Plugin<Config>
+    {
+        private EventManager manager;
+
+        public static MERDummyDoorInstaller Singleton;
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            manager = new EventManager();
+
+            Register();
+        }
+
+        public override void OnDisabled()
+        {
+            UnRegister();
+            manager = null;
+            Singleton = null;
+        }
+
+        void Register()
+        {
+            Maps.LoadingMap += manager.OnLoadMap;
+        }
+
+        void UnRegister()
+        {
+            Maps.LoadingMap -= manager.OnLoadMap;
+        }
+    }
+
+    public class EventManager
+    {
+        Config config = MERDummyDoorInstaller.Singleton.Config;
+
+        public void OnLoadMap(MapEditorReborn.Events.EventArgs.LoadingMapEventArgs ev)
+        {
+            if (!ev.IsAllowed) return;
+            int index = config.DummyDoorInstallingMaps.IndexOf(ev.NewMap.Name);
+            if (index != -1)
+            {
+                for (int i = 0; i < ev.NewMap.Doors.Count; i++)
+                {
+                    if (config.BlackListOfDummyDoor[index].Contains(i))
+                    {
+                        continue;
+                    }
+                    string str = "DoorLCZ";
+                    switch (ev.NewMap.Doors[i].DoorType)
+                    {
+                        case DoorType.HeavyContainmentDoor:
+                            str = "DoorHCZ";
+                            break;
+                        case DoorType.EntranceDoor:
+                            str = "DoorEZ";
+                            break;
+                    }
+                    MapEditorReborn.API.Features.Objects.SchematicObject @object = MapEditorReborn.API.Features.ObjectSpawner.SpawnSchematic(str, ev.NewMap.Doors[i].Position, Quaternion.Euler(ev.NewMap.Doors[i].Rotation));
+                    @object.gameObject.AddComponent<DummyDoor>().door = ev.NewMap.Doors[i];
+                }
+            }
+        }
+    }
+}
