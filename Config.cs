0912b41e5517ee6272614e98463a3dd1a99707a3
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-08-03 08:42:12 +0900

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -31,5 +31,25 @@ namespace MERDummyDoorInstaller
                 15
             }
         };
+        [Description("If turned on, it will search after specific event occur.")]
+        public bool AutoRun { get; set; } = false;
+        [Description("Generated, Round, Decont, Warhead")]
+        public List<EventList> AutoRunOnEventList { get; set; } = new List<EventList>
+        {
+            Config.EventList.Generated,
+        };
+        [Description("If you load massive schematic, extend delay.")]
+        public float AutoRunDelay { get; set; } = 2f;
+        [Description("If you build doors with external-plugin not MER, use this option.")]
+        public bool AutoRunWithEveryDoor { get; set; } = false;
+
+        [Serializable]
+        public enum EventList
+        {
+            Generated,
+            Round,
+            Decont,
+            Warhead
+        }
     }
 }
