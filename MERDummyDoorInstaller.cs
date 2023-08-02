71cc80f42b786357b15367aea3309173d918fd41
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-08-03 08:42:27 +0900

    Update MERDummyDoorInstaller.cs

--- MERDummyDoorInstaller.cs
+++ MERDummyDoorInstaller.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,10 @@ using Exiled.API.Features;
 using Exiled.API.Enums;
 using Exiled.Loader;
 using UnityEngine;
+using CustomCulling;
+using Interactables.Interobjects;
+using Interactables;
+using Interactables.Interobjects.DoorUtils;
 
 using Maps = MapEditorReborn.Events.Handlers.Map;
 
@@ -36,11 +40,19 @@ namespace MERDummyDoorInstaller
         void Register()
         {
             Maps.LoadingMap += manager.OnLoadMap;
+            Exiled.Events.Handlers.Map.Generated += manager.OnGen;
+            Exiled.Events.Handlers.Server.RoundStarted += manager.OnRound;
+            Exiled.Events.Handlers.Map.Decontaminating += manager.OnDecont;
+            Exiled.Events.Handlers.Warhead.Detonated += manager.OnAlpha;
         }
 
         void UnRegister()
         {
             Maps.LoadingMap -= manager.OnLoadMap;
+            Exiled.Events.Handlers.Map.Generated -= manager.OnGen;
+            Exiled.Events.Handlers.Server.RoundStarted -= manager.OnRound;
+            Exiled.Events.Handlers.Map.Decontaminating -= manager.OnDecont;
+            Exiled.Events.Handlers.Warhead.Detonated -= manager.OnAlpha;
         }
     }
 
@@ -74,6 +86,86 @@ namespace MERDummyDoorInstaller
                     @object.gameObject.AddComponent<DummyDoor>().door = ev.NewMap.Doors[i];
                 }
             }
+        }
+
+        public void OnGen()
+        {
+            if (config.AutoRunOnEventList.Contains(Config.EventList.Generated))
+            {
+                AutoRun();
+            }
+        }
+
+        public void OnRound()
+        {
+            if (config.AutoRunOnEventList.Contains(Config.EventList.Round))
+            {
+                AutoRun();
+            }
+        }
+
+        public void OnDecont(Exiled.Events.EventArgs.Map.DecontaminatingEventArgs ev)
+        {
+            if (ev.IsAllowed && config.AutoRunOnEventList.Contains(Config.EventList.Decont))
+            {
+                AutoRun();
+            }
+        }
+
+        public void OnAlpha()
+        {
+            if (config.AutoRunOnEventList.Contains(Config.EventList.Warhead))
+            {
+                AutoRun();
+            }
+        }
+
+        public void AutoRun()
+        {
+            if (!config.AutoRun) return;
+            MEC.Timing.CallDelayed(config.AutoRunDelay, () =>
+            {
+                if (!config.AutoRunWithEveryDoor)
+                {
+                    int index = config.DummyDoorInstallingMaps.IndexOf(MapEditorReborn.API.API.CurrentLoadedMap.Name);
+                    if (index != -1)
+                    {
+                        for (int i = 0; i < MapEditorReborn.API.API.CurrentLoadedMap.Doors.Count; i++)
+                        {
+                            if (config.BlackListOfDummyDoor[index].Contains(i))
+                            {
+                                continue;
+                            }
+                            string str = "DoorLCZ";
+                            switch (MapEditorReborn.API.API.CurrentLoadedMap.Doors[i].DoorType)
+                            {
+                                case DoorType.HeavyContainmentDoor:
+                                    str = "DoorHCZ";
+                                    break;
+                                case DoorType.EntranceDoor:
+                                    str = "DoorEZ";
+                                    break;
+                            }
+                            MapEditorReborn.API.Features.Objects.SchematicObject @object = MapEditorReborn.API.Features.ObjectSpawner.SpawnSchematic(str, MapEditorReborn.API.API.CurrentLoadedMap.Doors[i].Position, Quaternion.Euler(MapEditorReborn.API.API.CurrentLoadedMap.Doors[i].Rotation));
+                            @object.gameObject.AddComponent<DummyDoor>().door = MapEditorReborn.API.API.CurrentLoadedMap.Doors[i];
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (DoorLinkingRooms door in GameObject.FindObjectsOfType<DoorLinkingRooms>())
+                    {
+                        if (door.gameObject.TryGetComponent<BasicDoor>(out BasicDoor basicDoor) && basicDoor.Rooms.Length == 0)
+                        {
+                            string str = "DoorLCZ";
+                            if (door.gameObject.name.Contains("HCZ")) str = "DoorHCZ";
+                            if (door.gameObject.name.Contains("EZ")) str = "DoorEZ";
+                            MapEditorReborn.API.Features.Objects.SchematicObject @object = MapEditorReborn.API.Features.ObjectSpawner.SpawnSchematic(str, basicDoor.transform.position, basicDoor.transform.rotation);
+                            @object.gameObject.AddComponent<DummyDoor>().RealDoor = Door.Get(basicDoor);
+                        }
+                    }
+                }
+            });
         }
     }
 }
