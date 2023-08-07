0ceb406a1eee273888afe807898f8e553ec16fdd
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-08-07 17:30:59 +0900

    Add files via upload

---
+++ Advanced MER tools/HealthObject.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public class HealthObject : MonoBehaviour
+{
+    public float Health = 0;
+    public int ArmorEfficient = 0;
+    public DeadType DeadType = DeadType.Disappear;
+}
+
+[Serializable]
+public class HealthObjectDTO
+{
+    public float Health;
+    public int ArmorEfficient;
+    public DeadType DeadType;
+    public string ObjectId;
+}
+
+[Serializable]
+public enum DeadType
+{
+    Disappear,
+    GetRigidbody,
+    DynamicDisappearing,
+    Explode,
+    ResetHP
+}
+
+public class HealthObjectCompiler : MonoBehaviour
+{
+    private static readonly Config Config = SchematicManager.Config;
+
+    [MenuItem("SchematicManager/Compile Health Schematics", priority = -11)]
+    public static void OnCompile()
+    {
+        foreach (Schematic schematic in GameObject.FindObjectsOfType<Schematic>())
+        {
+            Compile(schematic);
+        }
+    }
+
+    public static void Compile(Schematic schematic)
+    {
+        string parentDirectoryPath = Directory.Exists(Config.ExportPath)
+            ? Config.ExportPath
+            : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                "MapEditorReborn_CompiledSchematics");
+        string schematicDirectoryPath = Path.Combine(parentDirectoryPath, schematic.gameObject.name);
+
+        if (!Directory.Exists(parentDirectoryPath))
+        {
+            Debug.LogError("Could Not find root object's compiled directory!");
+            return;
+        }
+
+        Directory.CreateDirectory(schematicDirectoryPath);
+
+        List<HealthObjectDTO> healthObjects = new List<HealthObjectDTO> { };
+
+        foreach (HealthObject health in schematic.transform.GetComponentsInChildren<HealthObject>())
+        {
+            string path = "";
+            Transform transform = health.transform;
+            while (true)
+            {
+                if (transform.parent == null) break;
+                for (int i = 0; i < transform.parent.childCount; i++)
+                {
+                    if (transform.parent.GetChild(i) == transform)
+                    {
+                        path += i.ToString();
+                    }
+                }
+                transform = transform.parent;
+            }
+            HealthObjectDTO dTO = new HealthObjectDTO
+            {
+                Health = health.Health,
+                ArmorEfficient = health.ArmorEfficient,
+                DeadType = health.DeadType,
+                ObjectId = path
+            };
+            healthObjects.Add(dTO);
+        }
+
+        string serializedData = JsonConvert.SerializeObject(healthObjects, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-HealthObjects.json"), serializedData);
+        Debug.Log("Successfully Imported HealthObjects.");
+    }
+}
---
+++ Advanced MER tools/Text.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public string Input;
+    public Vector3 DistanceBetweenChar;
+    public Vector3 SpacebarLength;
+
+    [Tooltip("Insert 26 letters to this. And becareful: object name is calling. 'A' -> call gameobject that have A, next a, lastly ' '.")]
+    public List<GameObject> Chars;
+
+    private void OnDrawGizmos()
+    {
+        if (Input == "" || (Input == previous && DistanceBetweenChar == p1 && SpacebarLength == p2)) return;
+        Vector3 vector = Vector3.zero;
+        duplicated.ForEach(x => DestroyImmediate(x));
+        for (int i = 0; i < Input.Length; i++)
+        {
+            GameObject game = Chars.Find(x => x.name.Equals(Input[i].ToString(), System.StringComparison.Ordinal));
+            if (game == null)
+            {
+                game = Chars.Find(x => x.name.Equals(Input[i].ToString(), System.StringComparison.OrdinalIgnoreCase));
+                if (game == null)
+                {
+                    vector += SpacebarLength;
+                    continue;
+                }
+            }
+            GameObject game1 = GameObject.Instantiate(game, this.transform);
+            game1.transform.localEulerAngles = Vector3.zero;
+            game1.transform.localScale = Vector3.one;
+            game1.transform.localPosition = vector;
+            vector += DistanceBetweenChar;
+            duplicated.Add(game1);
+        }
+        previous = Input;
+        p1 = DistanceBetweenChar;
+        p2 = SpacebarLength;
+    }
+
+    string previous;
+    Vector3 p1;
+    Vector3 p2;
+
+    List<GameObject> duplicated = new List<GameObject> { };
+}
