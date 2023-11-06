bb6f627bd571531266bd4e1bcf010364ca90fdd5
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-11-06 18:29:43 +0900

    Update InteractablePickup.cs

--- AdvancedMERTools (plugin)/InteractablePickup.cs
+++ AdvancedMERTools (plugin)/InteractablePickup.cs
@@ -265,21 +265,28 @@ namespace AdvancedMERTools
 
         string ApplyFormat(string context, Player player, Pickup pickup)
         {
-            context = context.Replace("{picker_i}", player.Id.ToString())
-            .Replace("{picker_name}", player.Nickname);
-            Vector3 vector3 = player.Position;
-            context = context.Replace("{a_pos}", string.Format("{0} {1} {2}", vector3.x, vector3.y, vector3.z));
-            context = context.Replace("{a_room}", player.CurrentRoom.RoomName.ToString())
-            .Replace("{a_zone}", player.CurrentRoom.Identifier.Zone.ToString())
-            .Replace("{a_role}", player.Role.Type.ToString());
-            vector3 = pickup.Transform.position;
-            context = context.Replace("{s_pos}", string.Format("{0} {1} {2}", vector3.x, vector3.y, vector3.z))
-            .Replace("{s_room}", Room.Get(this.transform.position).Type.ToString())
-            .Replace("{s_zone}", Room.Get(this.transform.position).Identifier.Zone.ToString());
-            if (player.CurrentItem == null)
-                context = context.Replace("{a_item}", "null");
-            else
-                context = context.Replace("{a_item}", player.CurrentItem.Type.ToString());
+            try
+            {
+                context = context.Replace("{picker_i}", player.Id.ToString())
+                .Replace("{picker_name}", player.Nickname)
+                .Replace("{a_role}", player.Role.Type.ToString());
+                Vector3 vector3 = player.Position;
+                context = context.Replace("{a_pos}", string.Format("{0} {1} {2}", vector3.x, vector3.y, vector3.z));
+                if (player.CurrentRoom != null)
+                    context = context.Replace("{a_room}", player.CurrentRoom.RoomName.ToString())
+                    .Replace("{a_zone}", player.CurrentRoom.Identifier.Zone.ToString());
+                vector3 = pickup.Transform.position;
+                context = context.Replace("{s_pos}", string.Format("{0} {1} {2}", vector3.x, vector3.y, vector3.z));
+                Room room = Room.Get(this.transform.position);
+                if (room != null)
+                    context = context.Replace("{s_room}", Room.Get(this.transform.position).Type.ToString())
+                    .Replace("{s_zone}", Room.Get(this.transform.position).Identifier.Zone.ToString());
+                if (player.CurrentItem == null)
+                    context = context.Replace("{a_item}", "null");
+                else
+                    context = context.Replace("{a_item}", player.CurrentItem.Type.ToString());
+            }
+            catch (Exception) { }
             return context;
         }
 
