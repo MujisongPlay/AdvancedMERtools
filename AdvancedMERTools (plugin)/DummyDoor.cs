0c839f49d0edf1f88a4f03dda18b5a02cab5725a
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2024-01-15 13:06:42 +0900

    Update DummyDoor.cs

--- AdvancedMERTools (plugin)/DummyDoor.cs
+++ AdvancedMERTools (plugin)/DummyDoor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,18 @@ using Exiled.API.Features;
 using MapEditorReborn.API.Features.Serializable;
 using MapEditorReborn.API.Features.Objects;
 using Exiled.API.Features.Doors;
-using Exiled.Events.Handlers;
 using Exiled.Events.EventArgs.Player;
+using SCPSLAudioApi;
+using SCPSLAudioApi.AudioCore;
+using System.IO;
+using Utf8Json;
+using System.Reflection.Emit;
+using System.Reflection;
+using PlayerRoles.FirstPersonControl;
+using PlayerRoles.FirstPersonControl.NetworkMessages;
+using Mirror;
+using PlayerRoles;
+using RelativePositioning;
 
 namespace AdvancedMERTools
 {
@@ -88,5 +99,203 @@ namespace AdvancedMERTools
                 Destroy(this.gameObject, 0.5f);
             }
         }
+    }
+
+    public class DummyGate : MonoBehaviour
+    {
+        void Start()
+        {
+            pickups = (from item in this.gameObject.GetComponentsInChildren<InventorySystem.Items.Pickups.ItemPickupBase>()
+                       select Exiled.API.Features.Pickups.Pickup.Get(item)).ToArray();
+            if (MapEditorReborn.API.API.CurrentLoadedMap != null && AdvancedMERTools.Singleton.Config.Gates.TryGetValue(MapEditorReborn.API.API.CurrentLoadedMap.Name, out List<GateSerializable> ser))
+            {
+                if (ser.Count > AdvancedMERTools.Singleton.dummyGates.Count)
+                {
+                    GateSerializable = ser[AdvancedMERTools.Singleton.dummyGates.Count];
+                    if (GateSerializable.IsOpened)
+                    {
+                        MEC.Timing.CallDelayed(3f, () => { IsOpened = true; });
+                    }
+                }
+            }
+            animator = this.transform.GetChild(1).GetComponent<Animator>();
+            AdvancedMERTools.Singleton.dummyGates.Add(this);
+            MEC.Timing.RunCoroutine(enumerator());
+        }
+
+        IEnumerator<float> enumerator()
+        {
+            yield return MEC.Timing.WaitUntilTrue(() => Round.IsStarted);
+            MEC.Timing.CallDelayed(0.3f, Apply);
+            yield break;
+        }
+
+        /*public void Apply()
+        //{
+            //AudioSource = AdvancedMERTools.MakeAudio("Dummy Gate #" + AdvancedMERTools.Singleton.dummyGates.Count.ToString());
+            //audioPlayer = AudioPlayerBase.Get(AudioSource);
+            //audioPlayer.BroadcastChannel = VoiceChat.VoiceChatChannel.Proximity;
+            //MEC.Timing.CallDelayed(0.3f, () =>
+            //{
+            //    AudioSource.roleManager.ServerSetRole(PlayerRoles.RoleTypeId.Tutorial, PlayerRoles.RoleChangeReason.None, PlayerRoles.RoleSpawnFlags.None);
+            //    audioPlayer.BroadcastChannel = VoiceChat.VoiceChatChannel.Proximity;
+            //});
+            //MEC.Timing.CallDelayed(0.4f, () =>
+            //{
+            //    FpcStandardRoleBase fpc = AudioSource.roleManager.CurrentRole as FpcStandardRoleBase;
+            //    if (fpc != null)
+            //    {
+            //        fpc.FpcModule.Noclip.IsActive = true;
+            //        AudioSource.transform.position = this.transform.position;
+            //        AudioSource.transform.localScale = Vector3.one * 0.1f;
+            //    }
+            //});
+
+            //ServerConsole.AddLog(this.transform.position.ToString());
+            //npc = Npc.Spawn("Gate", PlayerRoles.RoleTypeId.Tutorial, position: this.transform.position);
+            //PropertyInfo info = typeof(CentralAuth.PlayerAuthenticationManager).GetProperty("InstanceMode");
+            //info.SetValue(npc.ReferenceHub.authManager, CentralAuth.ClientInstanceMode.DedicatedServer);
+            //audioPlayer = AudioPlayerBase.Get(npc.ReferenceHub);
+            //MEC.Timing.CallDelayed(0.35f, () =>
+            //{
+            //    info.SetValue(npc.ReferenceHub.authManager, CentralAuth.ClientInstanceMode.ReadyClient);
+            //});
+            //MEC.Timing.CallDelayed(0.55f, () =>
+            //{
+            //    npc.Scale = Vector3.one * -0.1f;
+            //    npc.Position = this.transform.position;
+            //    (npc.RoleManager.CurrentRole as FpcStandardRoleBase).FpcModule.Noclip.IsActive = true;
+
+            //});
+            //MEC.Timing.CallDelayed(0.45f, () =>
+            //{
+            //    info.SetValue(npc.ReferenceHub.authManager, CentralAuth.ClientInstanceMode.DedicatedServer);
+            //});
+        //}*/
+
+        public void Apply()
+        {
+            ReferenceHub hub = AdvancedMERTools.MakeAudio(out int id);
+            MEC.Timing.CallDelayed(0.35f, () => 
+            {
+                hub.authManager.UserId = "ID_Dedicated";
+                //Central Core.
+                hub.authManager.NetworkSyncedUserId = "ID_Dedicated";
+                hub.nicknameSync.DisplayName = $"{id}-Gate";
+                hub.characterClassManager.GodMode = true;
+                hub.transform.localScale = Vector3.one * -0.01f;
+                foreach (Player player in Player.List)
+                {
+                    Server.SendSpawnMessage.Invoke(null, new object[]
+                    {
+                        hub.netIdentity,
+                        player.Connection
+                    });
+                }
+                FirstPersonMovementModule module = (hub.roleManager.CurrentRole as FpcStandardRoleBase).FpcModule;
+                //Preventer.
+                module.Position = this.transform.position - Vector3.up * 0.1f;
+                module.Motor.ReceivedPosition = new RelativePosition(this.transform.position - Vector3.up * 0.1f);
+                module.Noclip.IsActive = true;
+            });
+            //MEC.Timing.CallDelayed(0.45f, () =>
+            //{
+            //    PropertyInfo info = typeof(CentralAuth.PlayerAuthenticationManager).GetProperty("InstanceMode");
+            //    info.SetValue(hub.authManager, CentralAuth.ClientInstanceMode.DedicatedServer);
+            //    //hub.authManager.UserId = null;
+            //});
+            audioPlayer = AudioPlayerBase.Get(hub);
+        }
+
+        void Update()
+        {
+            if (Cooldown >= 0)
+            Cooldown -= Time.deltaTime;
+        }
+
+        public void OnPickingUp(SearchingPickupEventArgs ev)
+        {
+            if (pickups.Contains(ev.Pickup))
+            {
+                ev.IsAllowed = false;
+                if (GateSerializable != null)
+                {
+                    if (!GateSerializable.IsLocked && CheckPermission(ev.Player, GateSerializable.keycardPermissions))
+                    {
+                        goto IL_01;
+                    }
+                }
+                else
+                {
+                    goto IL_01;
+                }
+            }
+            return;
+        IL_01:;
+            if (Cooldown <= 0)
+            {
+                IsOpened = !IsOpened;
+            }
+        }
+
+        public bool CheckPermission(Exiled.API.Features.Player player, Interactables.Interobjects.DoorUtils.KeycardPermissions keycard)
+        {
+            if (keycard == Interactables.Interobjects.DoorUtils.KeycardPermissions.None)
+            {
+                return true;
+            }
+            if (player != null)
+            {
+                if (player.IsBypassModeEnabled)
+                {
+                    return true;
+                }
+                if (player.IsScp)
+                {
+                    return keycard.HasFlag(Interactables.Interobjects.DoorUtils.KeycardPermissions.ScpOverride);
+                }
+                if (player.CurrentItem == null)
+                {
+                    return false;
+                }
+                InventorySystem.Items.Keycards.KeycardItem keycardItem = player.CurrentItem.Base as InventorySystem.Items.Keycards.KeycardItem;
+                if (keycardItem != null)
+                {
+                    if (GateSerializable != null && GateSerializable.RequireAllPermission)
+                    {
+                        return (keycardItem.Permissions & keycard) == keycard;
+                    }
+                    return (keycardItem.Permissions & keycard) > Interactables.Interobjects.DoorUtils.KeycardPermissions.None;
+                }
+            }
+            return false;
+        }
+
+        public bool IsOpened 
+        {
+            get
+            {
+                return _IsOpened;
+            }
+            set
+            {
+                if (animator != null)
+                {
+                    audioPlayer.CurrentPlay = Path.Combine(Path.Combine(Paths.Configs, "Music"), value ? "GateOpen.ogg" : "GateClose.ogg");
+                    audioPlayer.Loop = false;
+                    audioPlayer.Play(-1);
+                    animator.Play(value ? "GateOpen" : "GateClose");
+                }
+                Cooldown = 3f;
+                _IsOpened = value;
+            }
+        }
+
+        GateSerializable GateSerializable;
+        public float Cooldown = 0f;
+        bool _IsOpened = false;
+        public Animator animator;
+        public Exiled.API.Features.Pickups.Pickup[] pickups;
+        AudioPlayerBase audioPlayer;
     }
 }
