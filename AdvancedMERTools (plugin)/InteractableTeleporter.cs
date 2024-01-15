514c4e50749da3640703a4e3d5e8aec6f57de010
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2024-01-15 13:08:37 +0900

    Add files via upload

---
+++ AdvancedMERTools (plugin)/InteractableTeleporter.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Exiled.API.Features;
+using Exiled.API.Features.Pickups;
+using Exiled.API.Features.Items;
+using MapEditorReborn.Events.EventArgs;
+using MapEditorReborn.API.Features.Objects;
+using MapEditorReborn.API.Features;
+using Exiled.CustomItems;
+using CommandSystem;
+using Utf8Json.Formatters;
+using RemoteAdmin;
+using Exiled.CustomItems.API.Features;
+
+namespace AdvancedMERTools
+{
+    public class InteractableTeleporter : AMERTInteractable
+    {
+        void Start()
+        {
+            this.Base = base.Base as ITDTO;
+            if (transform.TryGetComponent<TeleportObject>(out TO))
+            {
+                AdvancedMERTools.Singleton.InteractableTPs.Add(this);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (Player.TryGet(collider, out Player player) && Base.InvokeType.HasFlag(TeleportInvokeType.Collide))
+            {
+                RunProcess(player);
+            }
+        }
+
+        public void RunProcess(Player player)
+        {
+            foreach (IPActionType type in Enum.GetValues(typeof(IPActionType)))
+            {
+                if (Base.ActionType.HasFlag(type))
+                {
+                    switch (type)
+                    {
+                        case IPActionType.Disappear:
+                            Destroy(gameObject, 0.1f);
+                            break;
+                        case IPActionType.Explode:
+                            ExplodeModule.GetSingleton<ExplodeModule>().Execute(ExplodeModule.SelectList<ExplodeModule>(Base.ExplodeModules), this.transform, player.ReferenceHub);
+                            break;
+                        case IPActionType.PlayAnimation:
+                            if (modules.Count == 0)
+                            {
+                                modules = AnimationModule.GetModules(Base.animationDTOs, this.gameObject);
+                                if (modules.Count == 0)
+                                {
+                                    break;
+                                }
+                            }
+                            AnimationModule.GetSingleton<AnimationModule>().Execute(AnimationModule.SelectList<AnimationModule>(modules));
+                            break;
+                        case IPActionType.Warhead:
+                            foreach (WarheadActionType warhead in Enum.GetValues(typeof(WarheadActionType)))
+                            {
+                                if (Base.warheadActionType.HasFlag(warhead))
+                                {
+                                    switch (warhead)
+                                    {
+                                        case WarheadActionType.Start:
+                                            Warhead.Start();
+                                            break;
+                                        case WarheadActionType.Stop:
+                                            Warhead.Stop();
+                                            break;
+                                        case WarheadActionType.Lock:
+                                            Warhead.IsLocked = true;
+                                            break;
+                                        case WarheadActionType.UnLock:
+                                            Warhead.IsLocked = false;
+                                            break;
+                                        case WarheadActionType.Disable:
+                                            Warhead.LeverStatus = false;
+                                            break;
+                                        case WarheadActionType.Enable:
+                                            Warhead.LeverStatus = true;
+                                            break;
+                                    }
+                                }
+                            }
+                            break;
+                        case IPActionType.SendMessage:
+                            MessageModule.GetSingleton<MessageModule>().Execute(MessageModule.SelectList<MessageModule>(Base.MessageModules), Formatter, player, TO);
+                            break;
+                        case IPActionType.DropItems:
+                            DropItem.GetSingleton<DropItem>().Execute(DropItem.SelectList<DropItem>(Base.dropItems), this.transform);
+                            break;
+                        case IPActionType.SendCommand:
+                            CommandModule.GetSingleton<CommandModule>().Execute(CommandModule.SelectList<CommandModule>(Base.commandings), Formatter, player, TO);
+                            break;
+                        case IPActionType.GiveEffect:
+                            EffectGivingModule.GetSingleton<EffectGivingModule>().Execute(EffectGivingModule.SelectList<EffectGivingModule>(Base.effectGivingModules), player);
+                            break;
+                    }
+                }
+            }
+        }
+
+        void OnDestroy()
+        {
+            AdvancedMERTools.Singleton.InteractableTPs.Remove(this);
+        }
+
+        static readonly Dictionary<string, Func<object[], string>> Formatter = new Dictionary<string, Func<object[], string>>
+        {
+            { "{p_i}", vs => (vs[0] as Player).Id.ToString() },
+            { "{p_name}", vs => (vs[0] as Player).Nickname.ToString() },
+            { "{p_pos}", vs => { Vector3 pos = (vs[0] as Player).Transform.position; return string.Format("{0} {1} {2}", pos.x, pos.y, pos.z); } },
+            { "{p_room}", vs => (vs[0] as Player).CurrentRoom.RoomName.ToString() },
+            { "{p_zone}", vs => (vs[0] as Player).Zone.ToString() },
+            { "{p_role}", vs => (vs[0] as Player).Role.Type.ToString() },
+            { "{p_item}", vs => (vs[0] as Player).CurrentItem.Type.ToString() },
+            { "{o_pos}", vs => { Vector3 pos = (vs[1] as TeleportObject).transform.position; return string.Format("{0} {1} {2}", pos.x, pos.y, pos.z); } },
+            { "{o_room}", vs => (vs[1] as TeleportObject).CurrentRoom.RoomName.ToString() },
+            { "{o_zone}", vs => (vs[1] as TeleportObject).CurrentRoom.Zone.ToString() }
+        };
+
+        public new ITDTO Base;
+
+        public TeleportObject TO;
+
+        public List<AnimationModule> modules = new List<AnimationModule> { };
+    }
+}
---
+++ AdvancedMERTools (plugin)/Patches.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using Exiled.Loader;
+using UnityEngine;
+using CustomCulling;
+using Interactables.Interobjects;
+using Interactables;
+using Interactables.Interobjects.DoorUtils;
+using System.IO;
+using Utf8Json;
+using MapEditorReborn.API.Features;
+using MapEditorReborn.API.Features.Objects;
+using Exiled.Events.Features;
+using Exiled.Events;
+using PlayerRoles;
+using MapEditorReborn.API;
+using MapEditorReborn.API.Enums;
+using MapEditorReborn.API.Features.Serializable;
+using Exiled.API.Features.Doors;
+using HarmonyLib;
+using Mirror;
+using System.Reflection.Emit;
+using System.Reflection;
+using PlayerRoles.FirstPersonControl;
+using CommandSystem;
+using CommandSystem.Commands;
+using RemoteAdmin;
+
+using Maps = MapEditorReborn.Events.Handlers.Map;
+
+namespace AdvancedMERTools
+{
+    [HarmonyPatch(typeof(DoorObject), nameof(DoorObject.Init))]
+    public class DoorSpawnPatcher
+    {
+        static void Prefix(DoorObject __instance)
+        {
+            if (AdvancedMERTools.Singleton.Config.AutoRun && __instance.gameObject.TryGetComponent(out Interactables.Interobjects.BasicDoor basicDoor) && basicDoor.Rooms.Length == 0)
+            {
+                string str = "DoorLCZ";
+                if (__instance.gameObject.name.Contains("HCZ")) str = "DoorHCZ";
+                if (__instance.gameObject.name.Contains("EZ")) str = "DoorEZ";
+                SchematicObject @object = ObjectSpawner.SpawnSchematic(str, basicDoor.transform.position, basicDoor.transform.rotation, isStatic: false);
+                DummyDoor dummy = @object.gameObject.AddComponent<DummyDoor>();
+                AdvancedMERTools.Singleton.dummyDoors.Add(dummy);
+                dummy.RealDoor = Door.Get(basicDoor);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MapUtils), nameof(MapUtils.LoadMap), new Type[] { typeof(MapSchematic) })]
+    public class MapLoadingPatcher
+    {
+        static void Postfix(MapSchematic map)
+        {
+            string path = Path.Combine(MapEditorReborn.MapEditorReborn.MapsDir, map.Name + "-ITeleporters.json");
+            if (File.Exists(path))
+            {
+                List<ITDTO> iTDTOs = JsonSerializer.Deserialize<List<ITDTO>>(File.ReadAllText(path));
+                TeleportObject[] teleports = API.SpawnedObjects.Where(x => x is TeleportObject).Cast<TeleportObject>().ToArray();
+                foreach (ITDTO to in iTDTOs)
+                {
+                    int n = int.Parse(to.ObjectId);
+                    if (n > 0 && n <= teleports.Length)
+                    {
+                        InteractableTeleporter interactable = teleports[n - 1].gameObject.AddComponent<InteractableTeleporter>();
+                        interactable.Base = to;
+                    }
+                }
+            }
+        }
+    }
+}
