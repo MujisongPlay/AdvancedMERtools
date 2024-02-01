551aad20212a318f188756d5bf75b0f2d628cbab
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2024-02-02 02:02:22 +0900

    Update AutoScaler.cs

--- Advanced MER tools/AutoScaler.cs
+++ Advanced MER tools/AutoScaler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AutoScaler : MonoBehaviour
@@ -11,18 +12,15 @@ public class AutoScaler : MonoBehaviour
         pairs.Clear();
         foreach (Transform game in this.transform.GetComponentsInChildren<Transform>())
         {
-            if (game.transform.GetComponents<Component>().Length == 1) continue;
-            pairs.Add(game, new Values(game.position, game.eulerAngles, game.lossyScale));
+            Vector3 v1 = game.position;
+            Vector3 v2 = game.eulerAngles;
+            Vector3 v3 = game.lossyScale;
+            pairs.Add(game, new Values(IsEmpty(game.gameObject) && ResetPos ? Vector3.zero : new Vector3(v1.x, v1.y, v1.z),
+                IsEmpty(game.gameObject) && ResetRot ? Vector3.zero : new Vector3(v2.x, v2.y, v2.z), 
+                IsEmpty(game.gameObject) && ResetScl ? Vector3.one : new Vector3(v3.x, v3.y, v3.z)));
         }
         List<Transform> list = new List<Transform> { };
         list.AddRange(this.transform.GetComponentsInChildren<Transform>());
-        list.FindAll(x => x.transform.GetComponents<Component>().Length == 1 || x == this.transform).ForEach(x => 
-        {
-            if (ResetPos) x.localPosition = Vector3.zero;
-            if (ResetRot) x.localEulerAngles = Vector3.zero;
-            if (ResetScl) x.localScale = Vector3.one;
-            list.Remove(x);
-        });
         list.ForEach(x =>
         {
             Values values = pairs[x];
@@ -31,12 +29,38 @@ public class AutoScaler : MonoBehaviour
             x.localScale = values.scl;
             //x.localScale = operatorDivide(x.lossyScale, values.scl);
         });
+        //wait(list);
+    }
+
+    //static async Task wait(List<Transform> transforms)
+    //{
+    //    ForceStop = true;
+    //    await Task.Delay(1000);
+    //    transforms.ForEach(x =>
+    //    {
+    //        Values values = pairs[x];
+    //        x.position = values.pos;
+    //        x.eulerAngles = values.rot;
+    //        //x.localScale = operatorDivide(x.lossyScale, values.scl);
+    //    });
+    //    ForceStop = false;
+    //}
+
+    bool IsEmpty(GameObject game)
+    {
+        if (game == gameObject || game.GetComponents<Component>().Length == 1)
+            return true;
+        if (!game.TryGetComponent<MeshFilter>(out _) && !game.TryGetComponent<Light>(out _) && !game.TryGetComponent<PickupComponent>(out _))
+            return true;
+        return false;
     }
 
     private Vector3 operatorDivide(Vector3 vector, Vector3 vector1)
     {
         return new Vector3(vector.x / vector1.x, vector.y / vector1.y, vector.z / vector1.z);
     }
+
+    //static bool ForceStop = false;
 
     public bool Activate;
     public bool ResetPos;
