ff5fd359528b4d76aea1920329082a26efd691c0
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2025-01-23 23:09:11 +0900

    Update InteractableObject.cs

--- Advanced MER tools/InteractableObject.cs
+++ Advanced MER tools/InteractableObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.ComponentModel;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 //using NaughtyAttributes;
 using System;
 using System.IO;
@@ -10,12 +11,20 @@ using Newtonsoft.Json;
 
 public class InteractableObject : FakeMono
 {
-    public IODTO data = new IODTO();
+    public new IODTO data = new();
+    public new FIODTO ScriptValueData = new();
+    public override DTO _data { get => data; }
+    public override DTO _ScriptValueData { get => ScriptValueData; }
 }
 
 [Serializable]
 public class IODTO : DTO
 {
+    public override void OnValidate()
+    {
+        AnimationModules.ForEach(x => x.AnimatorAdress = PublicFunctions.FindPath(x.Animator));
+    }
+
     public KeyCode InputKeyCode;
     public float InteractionMaxRange;
     public IPActionType ActionType;
@@ -29,6 +38,42 @@ public class IODTO : DTO
     public List<EffectGivingModule> effectGivingModules;
     public List<AudioModule> AudioModules;
     public List<CGNModule> GroovieNoiseToCall;
+    public List<CFEModule> FunctionToCall;
+}
+
+[Serializable]
+public class FIODTO : DTO
+{
+    public override void OnValidate()
+    {
+        InteractionMaxRange.OnValidate();
+        Scp914Mode.OnValidate();
+        warheadActionType.OnValidate();
+        AnimationModules.ForEach(x => { x.parent = this; x.OnValidate(); x.AnimatorAdress = PublicFunctions.FindPath(x.Animator); });
+        MessageModules.ForEach(x => x.OnValidate());
+        dropItems.ForEach(x => x.OnValidate());
+        commandings.ForEach(x => x.OnValidate());
+        ExplodeModules.ForEach(x => x.OnValidate());
+        effectGivingModules.ForEach(x => x.OnValidate());
+        AudioModules.ForEach(x => x.OnValidate());
+        GroovieNoiseToCall.ForEach(x => x.OnValidate());
+        FunctionToCall.ForEach(x => x.OnValidate());
+    }
+
+    public KeyCode InputKeyCode;
+    public ScriptValue InteractionMaxRange;
+    public IPActionType ActionType;
+    public ScriptValue Scp914Mode;
+    public List<FAnimationDTO> AnimationModules;
+    public ScriptValue warheadActionType;
+    public List<FMessageModule> MessageModules;
+    public List<FDropItem> dropItems;
+    public List<FCommanding> commandings;
+    public List<FExplodeModule> ExplodeModules;
+    public List<FEffectGivingModule> effectGivingModules;
+    public List<FAudioModule> AudioModules;
+    public List<FCGNModule> GroovieNoiseToCall;
+    public List<FCFEModule> FunctionToCall;
 }
 
 public class InteractableObjectCompiler
@@ -60,22 +105,8 @@ public class InteractableObjectCompiler
 
         Directory.CreateDirectory(schematicDirectoryPath);
 
-        List<IODTO> interactables = new List<IODTO> { };
-
-        foreach (InteractableObject ip in schematic.transform.GetComponentsInChildren<InteractableObject>())
-        {
-            ip.data.AnimationModules.ForEach(x =>
-            {
-                x.AnimatorAdress = PublicFunctions.FindPath(x.Animator.transform);
-            });
-            ip.data.Code = ip.GetInstanceID();
-            ip.data.ObjectId = PublicFunctions.FindPath(ip.transform);
-            interactables.Add(ip.data);
-        }
-
-        string serializedData = JsonConvert.SerializeObject(interactables, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-
-        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-Objects.json"), serializedData);
+        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-Objects.json"), JsonConvert.SerializeObject(schematic.transform.GetComponentsInChildren<InteractableObject>().Where(x => !x.UseScriptValue).Select(x => x.data), Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
+        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-FObjects.json"), JsonConvert.SerializeObject(schematic.transform.GetComponentsInChildren<InteractableObject>().Where(x => x.UseScriptValue).Select(x => x.ScriptValueData).ToList(), Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, TypeNameHandling = TypeNameHandling.Auto }).Replace("Assembly-CSharp", "AdvancedMERTools"));
         Debug.Log("Successfully Imported Interactable Objects.");
     }
 }
