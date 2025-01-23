da8eb5a2a7167652c60c9ed9efe841f487e18787
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2025-01-23 23:08:17 +0900

    Update CustomCollider.cs

--- Advanced MER tools/CustomCollider.cs
+++ Advanced MER tools/CustomCollider.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Collections;
 using System.ComponentModel;
 using System.IO;
@@ -9,12 +10,20 @@ using UnityEngine;
 
 public class CustomCollider : FakeMono
 {
-    public CCDTO data = new CCDTO();
+    public new CCDTO data = new CCDTO();
+    public new FCCDTO ScriptValueData = new FCCDTO();
+    public override DTO _data { get => data; }
+    public override DTO _ScriptValueData { get => ScriptValueData; }
 }
 
 [Serializable]
 public class CCDTO : DTO
 {
+    public override void OnValidate()
+    {
+        AnimationModules.ForEach(x => x.AnimatorAdress = PublicFunctions.FindPath(x.Animator));
+    }
+
     public ColliderActionType ColliderActionType;
 	public CollisionType CollisionType;
     public DetectType DetectType;
@@ -28,6 +37,43 @@ public class CCDTO : DTO
 	public List<EffectGivingModule> effectGivingModules;
     public List<AudioModule> AudioModules;
     public List<CGNModule> GroovieNoiseToCall;
+    public List<CFEModule> FunctionToCall;
+}
+
+[Serializable]
+public class FCCDTO : DTO
+{
+    public override void OnValidate()
+    {
+        CollisionType.OnValidate();
+        DetectType.OnValidate();
+        ModifyHealthAmount.OnValidate();
+        warheadActionType.OnValidate();
+        AnimationModules.ForEach(x => { x.parent = this; x.OnValidate(); x.AnimatorAdress = PublicFunctions.FindPath(x.Animator); });
+        MessageModules.ForEach(x => x.OnValidate());
+        dropItems.ForEach(x => x.OnValidate());
+        commandings.ForEach(x => x.OnValidate());
+        ExplodeModules.ForEach(x => x.OnValidate());
+        effectGivingModules.ForEach(x => x.OnValidate());
+        AudioModules.ForEach(x => x.OnValidate());
+        GroovieNoiseToCall.ForEach(x => x.OnValidate());
+        FunctionToCall.ForEach(x => x.OnValidate());
+    }
+
+    public ColliderActionType ColliderActionType;
+    public ScriptValue CollisionType;
+    public ScriptValue DetectType;
+    public ScriptValue ModifyHealthAmount;
+    public List<FAnimationDTO> AnimationModules;
+    public ScriptValue warheadActionType;
+    public List<FMessageModule> MessageModules;
+    public List<FDropItem> dropItems;
+    public List<FCommanding> commandings;
+    public List<FExplodeModule> ExplodeModules;
+    public List<FEffectGivingModule> effectGivingModules;
+    public List<FAudioModule> AudioModules;
+    public List<FCGNModule> GroovieNoiseToCall;
+    public List<FCFEModule> FunctionToCall;
 }
 
 public class CustomColliderCompiler
@@ -59,22 +105,8 @@ public class CustomColliderCompiler
 
         Directory.CreateDirectory(schematicDirectoryPath);
 
-        List<CCDTO> interactables = new List<CCDTO> { };
-
-        foreach (CustomCollider ip in schematic.transform.GetComponentsInChildren<CustomCollider>())
-        {
-            ip.data.AnimationModules.ForEach(x => 
-            {
-                x.AnimatorAdress = PublicFunctions.FindPath(x.Animator.transform);
-            });
-            ip.data.Code = ip.GetInstanceID();
-            ip.data.ObjectId = PublicFunctions.FindPath(ip.transform);
-            interactables.Add(ip.data);
-        }
-
-        string serializedData = JsonConvert.SerializeObject(interactables, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-
-        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-Colliders.json"), serializedData);
+        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-Colliders.json"), JsonConvert.SerializeObject(schematic.transform.GetComponentsInChildren<CustomCollider>().Where(x => !x.UseScriptValue).Select(x => x.data), Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
+        File.WriteAllText(Path.Combine(schematicDirectoryPath, $"{schematic.gameObject.name}-FColliders.json"), JsonConvert.SerializeObject(schematic.transform.GetComponentsInChildren<CustomCollider>().Where(x => x.UseScriptValue).Select(x => x.ScriptValueData).ToList(), Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, TypeNameHandling = TypeNameHandling.Auto }).Replace("Assembly-CSharp", "AdvancedMERTools"));
         Debug.Log("Successfully Imported Custom Colliders.");
     }
 }
